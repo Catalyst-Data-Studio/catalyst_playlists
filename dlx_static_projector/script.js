9b61bf02d425ef87241ce0689e683fb58a2db1f3
Devin Bayly <baylyd@email.arizona.edu>
2025-02-13 11:32:06 -0700

    addign the dlx static projector page

---
+++ dlx_static_projector/script.js
@@ -0,0 +1,130 @@
+let sheetLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1SvlgwjDPvbz9tVZ1MTRq_AkVonfs-YWAoCUpgYFV8t5sSL7SN58lyAfWM9LURf6HAsurzU52f1kw/pub?gid=145287685&single=true&output=csv"
+// let overrideLinks = ['wide_yvette_viz_Womensrepresentationinpoliticsvizforsocialgood_WomeninPolitics_Dashboard1.png',
+//   'wide_sparsonsdataviz_viz_SixNations136YearHistorySportsVizSunday_FULLHISTORYOFTHESIXNATIONS_Dashboard1.png',
+//   'wide_neil.richards_viz_smallisbeautiful-TP_Dashboard52_Dashboard1.png',
+//   'wide_ken.flerlage_viz_AHistoryoftheWorldCup_WorldCup_Dashboard1.png']
+let overrideLinks=["https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Becker.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/BIG%20GAME%20BATTLE.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Commercial%20Market%20Outlook.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%201%281%29.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%201.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%205%20%282%29.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/DASHBOARD.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Fruit.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/FULL%20HISTORY%20OF%20THE%20SIX%20NATIONS.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Garbage%20in%20the%20Ocean.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Gender%26Ethnic%20disparities%20in%20Tech%20Companies.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/His%20Purple%20Majesty.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Internet%20nelle%20famiglie.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/INVISIBLE%20CONSEQUENCES.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/MAKOVERMONDAY%20-%20TRUMP%27S%20%27EXECUTIVE%20TIME%27.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Medigap.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/New%20York%20Rat%20Sightings.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/PlayerViz.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Ratios%20of%20Inequity.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Starcraft%20II%20Player%20Network.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Story%201.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Tableau%20de%20bord%201.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/The%20American%20Political%20Universe.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Um.....png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Women%20in%20Politics.png" ]
+overrideLinks=["https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Becker.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Commercial%20Market%20Outlook.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%201%281%29.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%205%20%282%29.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/DASHBOARD.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/FULL%20HISTORY%20OF%20THE%20SIX%20NATIONS.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Gender%26Ethnic%20disparities%20in%20Tech%20Companies.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/His%20Purple%20Majesty.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/INVISIBLE%20CONSEQUENCES.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/New%20York%20Rat%20Sightings.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/PlayerViz.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Ratios%20of%20Inequity.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/The%20American%20Political%20Universe.png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Um.....png" ,"https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Women%20in%20Politics.png" ]
+
+
+
+// let overrideLinks = ["https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Becker.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/BIG%20GAME%20BATTLE.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%205%20%282%29.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/BIG%20GAME%20BATTLE%281%29.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Dashboard%201%282%29.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/DASHBOARD.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Garbage%20in%20the%20Ocean.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/FULL%20HISTORY%20OF%20THE%20SIX%20NATIONS.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Gender%26Ethnic%20disparities%20in%20Tech%20Companies.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/His%20Purple%20Majesty.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/INVISIBLE%20CONSEQUENCES.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Internet%20nelle%20famiglie.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/MAKOVERMONDAY%20-%20TRUMP%27S%20%27EXECUTIVE%20TIME%27%281%29.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Ratios%20of%20Inequity.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/New%20York%20Rat%20Sightings.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/PlayerViz.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Starcraft%20II%20Player%20Network.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Um.....png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Tableau%20de%20bord%201.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/Women%20in%20Politics.png", "https://data.cyverse.org/dav-anon/iplant/home/baylyd/dlx_downloads/World%20Cup.png"]
+
+let outer = document.querySelector("#outer")
+let refreshSeconds = 30
+let refreshMS = refreshSeconds * 1000
+
+// set up the timer for the countdown text
+
+// get the number of the window group, and this will make sure we don't re-use any links
+// this might also mean that we need to set a reset on the window to sync it (both windows opened at different times will start their refresh timers separately)
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+async function triggerPlaylist() {
+
+  console.log("starting")
+
+  let holder = document.querySelector("#holder")
+  // make 4 smaller divs for the videos
+
+
+  let data = overrideLinks.map(e => ({ link: e, tag: "vizzies" }))
+  setTimeout(() => {
+    window.location.reload()
+  }, data.length * refreshMS)
+
+  //data = shuffle(data)
+  //slice skips the column header
+  let subdata = data
+  let indices = Array(subdata.length).fill(0).map((e, i) => i)
+
+  groupIndices = shuffle(indices)
+  function addToSubHolder(subholder, indices) {
+    let img
+    let video
+    let linkNumber = 0
+    for (let i of indices) {
+      let row = subdata[i]
+      let link = row.link
+      console.log("link is", link)
+      // this will ensure that the pages get to play for a certain amount of time
+
+      setTimeout(() => {
+        let playobject
+
+        if (video) { video.remove() }
+        if (img) { img.remove() }
+        img = document.createElement("img")
+
+        playobject = img
+
+        outer.append(playobject)
+
+        playobject.src = `https://data.cyverse.org/dav-anon/iplant/home/baylyd/screen_grabs/${link}`
+        // figure out the image dimensions here, and the introduce a slow scroll
+        img.onload = (e) => {
+          console.log(e)
+          console.log(img.size)
+          let height = img.height
+          let width = img.width
+          // get the size of the holder right now
+          let intervalid
+          let freq = 50
+          let divscale = refreshMS / freq
+          let bb = subholder.getBoundingClientRect()
+          let sheight = window.scrollHeight - bb.height
+          let sh = 1
+          let pos =0
+          intervalid = setInterval(() => {
+            pos+= sh
+            window.scrollTo(0,pos)
+            if (window.scrollTop > window.sheight || window.scrollTop <0 ) {
+              sh *=-1
+            }
+          }, freq)
+
+
+
+          setTimeout(() => {
+            clearInterval(intervalid)
+          }, refreshMS)
+
+        }
+      }, refreshMS * linkNumber)
+
+      linkNumber += 1
+
+
+    }
+
+  }
+  let indicesPerD = indices
+  addToSubHolder(outer, indices)
+
+}
+function slowroll() {
+  let start = 0
+  setInterval(() => {
+    start += 10
+    window.scroll({ top: start, behavior: "smooth" })
+  }
+    , 100)
+}
+window.onload = triggerPlaylist
+
