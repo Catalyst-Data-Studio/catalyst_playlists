5a532ac4e0fe562a687d62411b8406fdb8336c32
Devin Bayly <baylyd@email.arizona.edu>
2025-09-03 17:45:55 +0000

    finish adding poc based on single user sketches

--- creative_coding/load_apis_and_iframes.js
+++ creative_coding/load_apis_and_iframes.js
@@ -1,15 +1,27 @@
 // let url = <iframe src="https://editor.p5js.org/baylyd/full/h_ygl9vqE"></iframe>
 
-                  
+let iwidth = 500
+let iheight = 500
 async function grabSketches() {
     
-    let result = fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://editor.p5js.org/editor/baylyd/projects')}`)
+    let result =await fetch(`https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent('https://editor.p5js.org/editor/baylyd/projects')}`)
                     .then(response => {
                       if (response.ok) return response.json()
                       throw new Error('Network response was not ok.')
                     })
-                    .then(data => console.log(data.contents));
+                    
     console.log(result)
+    let container = document.querySelector("#container")
+    for (let sketch of result.slice(0,20)) {
+      let iframe = document.createElement("iframe")
+      let id = sketch.id
+      iframe.src = `https://editor.p5js.org/baylyd/full/${id}`
+      iframe.setAttribute("frameBorder","0")
+      iframe.setAttribute("width",iwidth)
+      iframe.setAttribute("height",iheight)
+      container.append(iframe)
+    }
+    
     return result
 
 }
@@ -18,14 +30,3 @@ function test() {
 }
 
 window.onload = grabSketches
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Your JavaScript code here will execute after the DOM is fully loaded,
-  // but before external resources like images and stylesheets are necessarily loaded.
-  console.log('The DOM is fully loaded and parsed!');
-  // Example: Manipulate elements within the body
-  const myElement = document.getElementById('container');
-  if (myElement) {
-    myElement.textContent = 'Content loaded!';
-  }
-});
